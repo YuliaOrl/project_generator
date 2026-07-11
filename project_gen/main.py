44e00b1397325e7d56534438f8f3ef0fecc90b44
Yulia <orbitalik32@gmail.com>
2026-07-11 13:27:54 +0300

    Добавлен генератор фикстур и тестов с использованием Jinja

--- project_gen/main.py
+++ project_gen/main.py
@@ -1,4 +1,6 @@
 import click
+from project_gen.internal.generator import Generator
+from project_gen.internal.test_generator import TestsGenerator
 from project_gen.utils.download import init
 from project_gen.utils.generate import generate
 from project_gen.utils.utils import setup
@@ -19,6 +21,8 @@ def setup_command(template: str | None) -> None:
 @cli.command("generate")
 def generate_command() -> None:
     generate()
+    Generator().generate()
+    TestsGenerator().generate()
 
 
 @cli.command("init")
